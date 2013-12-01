7a17ccbcaea8b58967113d4701593de24c39d6a1
Lars Fischer <fischer@wiwi.uni-siegen.de>
2013-12-01 23:49:33 +0100

    makedepend is now recursive

--- bin/initLecture.sh
+++ bin/initLecture.sh
@@ -10,7 +10,6 @@
 # * Interactive Installation (Ask for values)
 # * Installation by Configuration (key=value)
 # * Different templates/document structures
-# * Preparation of lecture files
 # * Check for libraries
 ###########################################################################
 
@@ -29,22 +28,24 @@ interval="Winter Term 2013/14"
 declare -A bodyreplace=()
 declare -A titlereplace=()
 
-while getopts cs:d:t:m: name
+while getopts cs:d:t:m:i: name
 do
     case $name in
     d)    dstdir="$OPTARG";;
     s)    srcdir="$OPTARG";;
     c)    copyall=true;;
     t)    title="$OPTARG"
-    	  bodyreplace["!!!TITLE!!!"]="${OPTARG}" 
-    	  titlereplace["!!!TITLE!!!"]="${OPTARG}" ;;
+    	  bodyreplace["!!!TITLE!!!"]="${OPTARG}" ;;
     i)    interval="$OPTARG"
-    	  bodyreplace["!!!TERM!!!"]="${OPTARG}" 
-    	  titlereplace["!!!TERM!!!"]="${OPTARG}" ;;
+    	  bodyreplace["!!!TERM!!!"]="${OPTARG}" ;;
     ?)   printf "Usage: %s: [-c] [-t title] [-i time interval/semester] [-s srcdir] [-d dstdir] [lectures... (not yet functional)]\n" $0
           exit 2;;
     esac
 done
+makereplace=${bodyreplace}
+titlereplace=${bodyreplace}
+
+echo $titlereplace
 echo "Install from ${srcdir}" >&2
 echo "Install to ${dstdir}" >&2
 
@@ -64,11 +65,11 @@ askproceed() {
 
 prepareFile() {
 	file=$1
-	replacements=$2
-	echo "preparing ${file}"
+	declare -A replacements=$2
+	echo $replacements
 	for i in ${!replacements[@]}; do
 		echo -e "\treplacing ${i} with ${replacements[${i}]}"
-		sed -i "/${i}/${replacements[${i}]}/" ${file}
+		sed -i "s/${i}/${replacements[${i}]}/" ${file}
 	done
 }
 
@@ -77,7 +78,7 @@ declare -a linkfiles=("Lecture_Notes.tex"  "Lecture_Slides.tex" "bin/Makefile" "
 # template files to be copied to the directory to be changed
 declare -a copyfiles=("body.tex" "title.tex" "introduction.tex") #"body.tex", "titlepage.tex" 
 
-declare -A preparefiles=(["body.tex"]=${bodyreplace} ["title.tex"]=${titlereplace})
+declare -A preparefiles=(["body.tex"]=${bodyreplace} ["title.tex"]=${titlereplace} ["Makefile"]=${makereplace} )
 
 if [ -z "${copyall}" ] ; then
 	echo "Now linking files ${linkfiles[@]}."
@@ -85,22 +86,31 @@ if [ -z "${copyall}" ] ; then
 	askproceed
 
 	for lf in ${linkfiles[@]}; do
+	        echo "Linking ${lf}"
 		ln -s "${srcdir}/${lf}" "${dstdir}/`basename ${lf}`"
 	done
 else
 	echo "Now copying files ${linkfiles} ${copyfiles}."
 	askproceed
         for lf in ${linkfiles[@]}; do
-                cp -i "${srcdir}/${lf}" "${dstdir}/${lf}"
+	        echo "Copying ${lf}"
+                cp -i "${srcdir}/${lf}" "${dstdir}/`basename ${lf}`"
         done
 fi
+
 for cf in ${copyfiles[@]}; do
-	cp -i "${srcdir}/${cf}" "${dstdir}/${cf}"
+        echo "Copying ${cf}"
+	cp -i "${srcdir}/${cf}" "${dstdir}/`basename ${cf}`"
 done
 
+echo "preparation not yet working"
+exit
+for pf in ${!preparefiles[@]}; do
+       DSTFILE="${pf}"
+       echo "Preparing $DSTFILE with ${preparefiles[${DSTFILE}]}"
 
-for pf in ${preparefiles[@]}; do
-	prepareFile "${dstdir}/${pf}" ${preparefiles[${pf}]}
+       
+#	prepareFile "${dstdir}/${DSTFILE}" ${preparefiles[${pf}]}
 done
 
 
